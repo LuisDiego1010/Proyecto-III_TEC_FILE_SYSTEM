731fcc32f15e549d5f2a9eecfc07e7543b0d2976
LuisDiego1010 <guisogarcia1010@estudiantec.cr>
2021-06-21 17:29:49 -0600

    Interface

--- src/CE programs/CeSearch.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by isaac on 19/6/21.
-//
-
-#include "CeSearch.h"
---
+++ src/CE programs/main.cpp
@@ -0,0 +1,12 @@
+//
+// Created by diegoubuntu on 21/6/21.
+//
+
+#include <iostream>
+#include "Interface.h"
+
+int main() {
+    Interface mainInterface;
+    mainInterface.show();
+    return 0;
+}
